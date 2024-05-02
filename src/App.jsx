7aa3b1d7912308fb8b8f87d31c298427691bb487
Leonardo Lodi <your_email@address>
2024-05-02 10:37:56 +0100

    add apps route and its link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import './App.css';
+
 import { useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import { appsData, productsData } from './data.js';
+
 import Home from './pages/Home';
 import ProductsPage from './pages/Products';
-import './App.css';
+import AppsPage from './pages/Apps/index.jsx';
 
 export default function App() {
   const [products, setProducts] = useState(productsData);
@@ -15,23 +18,39 @@ export default function App() {
     <div className="App">
       <header>
         <h1>Apple Shop</h1>
+
         <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
+
             <li>
               <Link to="/products">Products</Link>
+            </li>
+
+            <li>
+              <Link to="/apps">Apps</Link>
             </li>
           </ul>
         </nav>
       </header>
+
       <Routes>
         <Route
           path="/products"
           element={<ProductsPage products={products} />}
         />
-        <Route path="/" element={<Home />} />
+
+        <Route 
+          path="/" 
+          element={<Home />} 
+        />
+
+        <Route 
+          path="/apps" 
+          element={<AppsPage apps={apps} />} 
+        />
       </Routes>
     </div>
   );
